2834ec60f2b88a1662e62569ed348cb08457c952
akipopo14 <108212530+akipopo14@users.noreply.github.com>
2022-07-15 10:45:02 +0900

    ローディング画面追加

--- js/script.js
+++ js/script.js
@@ -100,3 +100,18 @@ $(function () {
     return false;
   });
 });
+
+$(function(){
+  //読み込みが完了したら実行する
+  $(window).on('load',function(){
+    //ローディングアニメーションをフェードアウト
+    $('.loader').delay(600).fadeOut(600);
+    //背景色をフェードアウト
+    $('.loader-bg').delay(900).fadeOut(800);
+});
+
+  //ページの読み込みが完了してなくても7秒後にアニメーションを非表示にする
+  setTimeout(function(){
+    $('.loader-bg').fadeOut(600);
+  },7000);
+});
